da249f351aa2aa68acdd3c6d27c918429d1289da
Kian <48360239+kcmath@users.noreply.github.com>
2023-06-11 07:26:03 -0400

    Create ex-001.cpp

---
+++ cc/ex-001.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class UnionFind {
+private:
+    vector<int> parent;
+    vector<int> rank;
+
+public:
+    UnionFind(int n) {
+        parent.resize(n);
+        rank.resize(n, 0);
+
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+        }
+    }
+
+    int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    void unite(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX == rootY) {
+            return;
+        }
+
+        if (rank[rootX] < rank[rootY]) {
+            parent[rootX] = rootY;
+        } else if (rank[rootX] > rank[rootY]) {
+            parent[rootY] = rootX;
+        } else {
+            parent[rootY] = rootX;
+            rank[rootX]++;
+        }
+    }
+};
+
+unordered_map<int, int> findConnectedComponents(vector<vector<int>>& graph) {
+    int numVertices = graph.size();
+    UnionFind uf(numVertices);
+    unordered_map<int, int> componentID;
+
+    for (int vertex = 0; vertex < numVertices; ++vertex) {
+        for (int neighbor : graph[vertex]) {
+            uf.unite(vertex, neighbor);
+        }
+    }
+
+    int componentCounter = 0;
+    for (int vertex = 0; vertex < numVertices; ++vertex) {
+        int root = uf.find(vertex);
+        if (componentID.find(root) == componentID.end()) {
+            componentID[root] = componentCounter++;
+        }
+        componentID[vertex] = componentID[root];
+    }
+
+    return componentID;
+}
+
+int main() {
+    int numVertices = 9;
+    vector<vector<int>> graph(numVertices);
+
+    // Adding edges to the graph
+    graph[0] = {1, 2};
+    graph[1] = {0, 2};
+    graph[2] = {0, 1};
+    graph[3] = {4, 5};
+    graph[4] = {3, 5};
+    graph[5] = {3, 4};
+    graph[6] = {7};
+    graph[7] = {6};
+    graph[8] = {};  // isolated vertex
+
+    // Find connected components using Union-Find
+    unordered_map<int, int> componentID = findConnectedComponents(graph);
+
+    // Output the vertex tagging
+    cout << "Vertex Component ID:" << endl;
+    for (int vertex = 0; vertex < numVertices; ++vertex) {
+        cout << "Vertex " << vertex << ": Component " << componentID[vertex] << endl;
+    }
+
+    return 0;
+}
